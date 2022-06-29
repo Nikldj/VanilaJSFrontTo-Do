5a88d5eff2edc989b16d3ac1fa81834619a4a16d
Nikita Rudakov <rudakov.exceedteam@gmail.com>
2022-06-29 17:33:50 +0300

    feature/fix_project

--- script.js
+++ script.js
@@ -1,232 +1,204 @@
-let allTask_2 = [];
 let allTask = [];
 let input = null;
-let value = '';
+let value = "";
 let edit = false;
-let index_el = 0;
-let check_index = false;
+let checkItem = 0;
 
-window.onload = async function init() {
-  input = document.getElementById('input_task');
-  input.addEventListener('change', UpdateValue)
-  const response = await fetch('http://localhost:8000/allTasks', {
-    method: 'GET'
+window.onload = async () => {
+  input = document.getElementById("input_task");
+  input.addEventListener("input", (elem) => (value = elem.target.value));
+  const response = await fetch("http://localhost:8000/allTasks", {
+    method: "GET",
   });
   let result = await response.json();
   allTask = result.data;
   render();
-}
-onClickButton = async () => {
+};
 
-  if (value == "") {
-    alert("Введите значение!")
-  } else {
-    if (check_index == false) {
-      allTask.push({
-        text: value,
-        isCheck: false,
-      });
-
-
-      const response = await fetch('http://localhost:8000/createTask', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json;charset=utf-8',
-          'Access-Control-Allow-Origin': '*'
-        },
-        body: JSON.stringify({
-          text: value,
-          isCheck: false,
-        })
-      });
-      let result = await response.json();
-      allTask = result.data;
-      console.log("allTask", allTask);
-
-      value = '';
-      input.value = null;
-      render();
-    } else {
-      alert('Отредактируйте!')
-    }
+const AddEnter = (event) => {
+  if(event.key === "Enter"){
+    addTask();
   }
 }
 
-const UpdateValue = (elem) => {
-  value = elem.target.value;
-}
+const addTask = async () => {
+  if (!value) {
+    alert("Введите текст!");
+    return;
+  }
+  if (edit) {
+    alert("Отредактируйте!");
+    return;
+  }
 
-const sortArr = () =>{
-  allTask_2 = [];
-  for (let i = 0; i < allTask.length; i++) {
-    if (allTask[i].isCheck == true) {
-      allTask_2.push(allTask[i]);
-      delete allTask[i];
-    }
-  }
-  allTask = allTask.filter(task => task.isCheck == false)
-  localStorage.setItem('tasks', JSON.stringify(allTask));
-  for (let i = 0; i < allTask_2.length; i++) {
-    if (allTask_2[i].isCheck == true) {
-      allTask.push(allTask_2[i]);
-    }
-  }
-  console.log(allTask);
-}
+  const response = await fetch("http://localhost:8000/createTask", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify({
+      text: value,
+      isCheck: false,
+    }),
+  });
+
+  let result = await response.json();
+  allTask = result.data;
+  value = null;
+  input.value = null;
+  render();
+};
+
+const sortArr = () => {
+  allTask.sort((a, b) => a.isCheck - b.isCheck);
+};
 
 const render = () => {
-  const content = document.getElementById('content_page');
+  const content = document.getElementById("content_page");
+
   while (content.firstChild) {
     content.removeChild(content.firstChild);
   }
+
   sortArr();
 
-  localStorage.setItem('tasks', JSON.stringify(allTask));
-
+  localStorage.setItem("tasks", JSON.stringify(allTask));
 
   allTask.forEach((element, index) => {
-
-    const container = document.createElement('div');
+    const container = document.createElement("div");
     container.id = `task-${index}`;
-    container.className = 'task_container';
-    const checkbox = document.createElement('input');
+    container.className = "task_container";
+    const checkbox = document.createElement("input");
+    checkbox.className = "checkInput";
     checkbox.type = "checkbox";
     checkbox.checked = element.isCheck;
-
-    checkbox.onchange = function () {
-      if (onChangeCheck(index)) {
-        checkbox.checked = false;
-      }
-    }
+    checkbox.onchange = () => onChangeCheck(index);
     container.appendChild(checkbox);
 
-    const text = document.createElement('p');
+    const text = document.createElement("p");
     text.innerText = element.text;
-    text.className = element.isCheck ? "text_task done_task" : "text_task"
+    text.className = element.isCheck ? "text_task done_task" : "text_task";
     container.appendChild(text);
 
-    const img = document.createElement('img');
-    img.src = 'img/edit_2.png';
-    img.className = 'img_edit';
-    img.onclick = function () {
-      EditTask(index);
-    }
     if (!allTask[index].isCheck) {
-      container.appendChild(img);
+      const editButton = document.createElement("img");
+      editButton.src = "img/edit_2.png";
+      editButton.className = "img_edit";
+      editButton.onclick = () => EditTask(index);
+      container.appendChild(editButton);
     }
 
+    const deleteButton = document.createElement("img");
+    deleteButton.src = "img/close.png";
+    deleteButton.className = "img_close";
+    deleteButton.onclick = () => DeleteTask(index);
 
-    const img_2 = document.createElement('img');
-    img_2.src = 'img/close.png';
-    img_2.className = 'img_close';
-    img_2.onclick = function () {
-      DeleteTask(index);
-    }
-    container.appendChild(img_2);
+    container.appendChild(deleteButton);
     content.appendChild(container);
   });
-}
+};
 
 const onChangeCheck = async (index) => {
-  if (check_index == false) {
-    allTask[index].isCheck = !allTask[index].isCheck;
+  let { id, isCheck } = allTask[index];
+  isCheck = !isCheck;
 
-    const response = await fetch('http://localhost:8000/updateTask', {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json;charset=utf-8',
-        'Access-Control-Allow-Origin': '*'
-      },
-      body: JSON.stringify({
-        id: allTask[index].id,
-        text: allTask[index].text,
-        isCheck: allTask[index].isCheck,
-      })
-    });
-    let result = await response.json();
-    allTask = result.data;
-    localStorage.setItem('tasks', JSON.stringify(allTask));
-    render();
-  } else {
-    alert('Отредактируйте');
-    return true;
-  }
+  const response = await fetch("http://localhost:8000/updateTask", {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify({
+      id,
+      isCheck,
+    }),
+  });
 
-}
+  let result = await response.json();
+  allTask = result.data;
+  render();
+};
 
 const DeleteTask = async (index) => {
-  if (check_index == false) {
-
-
-    const response = await fetch(`http://localhost:8000/deleteTask?id=${allTask[index].id}`, {
-      method: 'DELETE',
-      headers: {
-        'Content-Type': 'application/json;charset=utf-8',
-        'Access-Control-Allow-Origin': '*'
-      },
-    });
-    let result = await response.json();
-    allTask = result.data;
-
-    localStorage.setItem('tasks', JSON.stringify(allTask));
-    render();
-  } else {
-    alert('Отредактируйте!')
+  if (edit) {
+    alert("Отредактируйте!");
+    return;
   }
 
-}
+  const response = await fetch(
+    `http://localhost:8000/deleteTask?id=${allTask[index].id}`,
+    {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+        "Access-Control-Allow-Origin": "*",
+      },
+    }
+  );
+  let result = await response.json();
+  allTask = result.data;
+
+  render();
+};
 
 const EditTask = async (index) => {
-  const img_2 = document.querySelectorAll('.img_edit')[index];
-  const img_3 = document.querySelectorAll('.img_close')[index];
+  const text = document.querySelectorAll(".text_task")[index];
+  const input = document.createElement("input");
+  const textReplace = document.createElement("p");
+  const imgEdit = document.querySelectorAll(".img_edit")[index];
+  const imgDelete = document.querySelectorAll(".img_close")[index];
+  const inputReplace = document.getElementById("inputReplace");
+  const checkbox2 = document.querySelectorAll(".checkInput");
+
   if (edit) {
-    const input = document.getElementById('input_replace');
-    const text = document.createElement('p');
-    text.className = "text_task";
-    if (!input.value) {
-      alert("Введите значение!")
+    if (checkItem != index) {
+      alert("Отредактируйте!");
+      return;
     }
-    else {
-      if (index_el != index) {
-        alert('Отредактируйте!');
-      } else {
-        text.innerText = input.value;
-        allTask[index].text = input.value;
+    textReplace.innerText = inputReplace.value;
+    allTask[index].text = inputReplace.value;
+    let { id, text } = allTask[index];
 
-        const response = await fetch('http://localhost:8000/updateTask', {
-          method: 'PATCH',
-          headers: {
-            'Content-Type': 'application/json;charset=utf-8',
-            'Access-Control-Allow-Origin': '*'
-          },
-          body: JSON.stringify({
-            id: allTask[index].id,
-            text: input.value,
-            isCheck: allTask[index].isCheck,
-          })
-        });
+    const response = await fetch("http://localhost:8000/updateTask", {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({
+        id,
+        text,
+      }),
+    });
 
-        let result = await response.json();
-        allTask = result.data;
-        localStorage.setItem('tasks', JSON.stringify(allTask));
+    textReplace.className = "text_task";
+    inputReplace.replaceWith(textReplace);
 
-        input.replaceWith(text);
-        edit = false;
-        img_2.src = 'img/edit_2.png';
-        img_3.style.display = 'block';
-        check_index = false;
+    imgEdit.src = "img/edit_2.png";
+    imgDelete.style.display = "block";
+    edit = false;
+
+    checkbox2.forEach((element) => {
+      if (!element.checked) {
+        element.disabled = false;
       }
-    }
+    });
   } else {
-    const text = document.querySelectorAll('.text_task')[index];
-    const input = document.createElement('input');
-    img_2.src = 'img/done.png';
-    img_3.style.display = 'none';
-    input.id = 'input_replace';
-    input.value += text.innerText;
+    imgEdit.src = "img/done.png";
+    imgDelete.style.display = "none";
+
+    input.id = "inputReplace";
+    input.value = allTask[index].text;
     text.replaceWith(input);
     edit = true;
-    index_el = index;
-    check_index = true;
+    checkItem = index;
+
+    checkbox2.forEach((element) => {
+      if (!element.checked) {
+        element.disabled = true;
+        element.checked = false;
+      }
+    });
   }
-}
+};
